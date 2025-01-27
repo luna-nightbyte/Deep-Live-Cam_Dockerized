f7d7c7880c3462c80cea0317b8085055e0ce29d7
luna-nightbyte <lunawifu18@gmail.com>
2025-01-27 21:20:17 +0100

    Modified docker and startup script to use .env file for all settings.

--- run.sh
+++ run.sh
@@ -1,61 +1,49 @@
 #!/bin/bash
+
+# Uncomment below if onnxruntime fails. Once sucessful, comment out again.
 # pip uninstall -y onnxruntime-gpu
 # pip install onnxruntime onnxruntime-gpu==1.18.0 insightface
-### Output paths
-root_output="output"
-source_dir="${root_output}/source_files" 
-target_dir="${root_output}/target_files"
-output_dir="${root_output}/output_files"
-# Function to process a single file
-process_file() {
-    local source_file="$1"
-    local target_file="$2"
-    local output_file="${output_dir}"
 
-    local cmd="python3 run.py \
-        -sf \"${source_file}\" \
-        -tf \"${target_file}\" \
-        -of \"${output_file}\" \
-        --max-memory ${max_mem} \
-        --many-faces \
-        --keep-fps \
-        --frame-processor ${frame_processor} \
-        --execution-threads ${threads} \
-        --max-memory ${max_mem}" \
 
-    if [[ "${USE_VIDEO_ARGS}" == true ]]; then
-        cmd+=" --video-encoder ${video_encoder} --video-quality ${video_quality} --keep-fps --keep-audio"
-    fi
-
-    if [[ "${MANY_FACES}" == true ]]; then
-        cmd+=" --many-faces"
-    fi
-    if [[ "${USE_GPU}" != false ]]; then
-        cmd+=" --execution-provider ${USE_GPU}"
-    fi
-    if [[ "${server_only}" != false ]]; then
-        cmd+=" -server"
-    fi
-
-    if [[ "${DEBUG}" == true ]]; then
-        echo "Running command: $cmd"
-    fi
-
-    eval $cmd
-
-    if [[ $? -ne 0 ]]; then
-        echo "Error: Command failed for file: $source_file"
-        exit 1
-    fi
-}
-
-# Startup
-
-## Create workdir folders
-mkdir -p "${output_dir}"
-
-if [[ "${DEBUG}" == true ]]; then
-    debug_info
+if [ -f .env ]; then
+    source .env
+else
+    echo ".env file not found! Copy [ example.env -> .env ] and make sure all settings are correct."
+    exit 1
 fi
-process_file "$source_dir" "$target_dir"
+
+cmd="python3 run.py \
+    -sf \"${SOURCE_FOLDER}\" \
+    -tf \"${TARGET_FOLDER}\" \
+    -of \"${OUTPUT_FOLDER}\" \
+    --max-memory ${MAX_MEM} \
+    --many-faces \
+    --keep-fps \
+    --frame-processor {${FRAME_PROCESSOR}} \
+    --execution-threads ${THREADS}"
+
+if [[ "${USE_VIDEO_ARGS}" == true ]]; then
+    cmd+=" \
+    --video-encoder ${VIDEO_ENCODER} \
+     --video-quality ${VIDEO_QUALITY} \
+     --keep-fps --keep-audio"
+fi
+if [[ "${MANY_FACES}" == true ]]; then
+    cmd+=" \
+    --many-faces"
+fi
+if [[ "${USE_GPU}" != false ]]; then
+    cmd+=" \
+    --execution-provider ${RUNTIME}"
+fi
+if [[ "${SERVER_ONLY}" != false ]]; then
+    cmd+=" \
+    -server"
+fi
+eval $cmd
+if [[ $? -ne 0 ]]; then
+    echo "Error: Command failed for file: $source_file"
+    exit 1
+fi
+
 

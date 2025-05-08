2e21a7f62068782ad5ff277775f2400a0c644d0a
luna-nightbyte <lunawifu18@gmail.com>
2025-05-08 14:57:27 +0200

    Added dockerfiles

---
+++ docker_script.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Uncomment below if onnxruntime fails. Once sucessful, comment out again.
+# pip uninstall -y onnxruntime-gpu
+# pip install onnxruntime onnxruntime-gpu==1.18.0 insightface
+
+# pip uninstall basicsr -y
+# pip install git+https://github.com/xinntao/BasicSR.git@master
+
+# pip uninstall gfpgan -y
+# pip install git+https://github.com/TencentARC/GFPGAN.git@master
+
+
+if [ -f .env ]; then
+    source .env
+else
+    echo ".env file not found! Copy [ example.env -> .env ] and make sure all settings are correct."
+    exit 1
+fi
+
+cmd="python3 run.py \
+    -sf \"${SOURCE_FOLDER}\" \
+    -tf \"${TARGET_FOLDER}\" \
+    -of \"${OUTPUT_FOLDER}\" \
+    --max-memory ${MAX_MEM} \
+    --many-faces \
+    --keep-fps \
+    --frame-processor ${FRAME_PROCESSOR} \
+    --execution-threads ${THREADS}"
+
+if [[ "${USE_VIDEO_ARGS}" == true ]]; then
+    cmd+=" \
+    --video-encoder ${VIDEO_ENCODER} \
+     --video-quality ${VIDEO_QUALITY} \
+     --keep-fps --keep-audio"
+fi
+if [[ "${MANY_FACES}" == true ]]; then
+    cmd+=" \
+    --many-faces"
+fi
+if [[ "${USE_GPU}" != false ]]; then
+    cmd+=" \
+    --execution-provider ${RUNTIME}"
+fi
+if [[ "${SERVER_ONLY}" != false ]]; then
+    cmd+=" \
+    -server"
+fi
+eval $cmd
+if [[ $? -ne 0 ]]; then
+    echo "Error: Command failed for file: $source_file"
+    exit 1
+fi
